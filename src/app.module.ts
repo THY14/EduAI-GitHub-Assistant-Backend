bfe5129c475d65f859767e1711b90f1d92bbccb4
methy <methymuy@gmail.com>
2026-01-12 16:15:26 +0700

    test_deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
+import { AppController } from '../src/app.controller';
 // import { ClassroomsModule } from './modules/Classrooms/classrooms.module';
 
 @Module({
   imports: [UsersModule, AuthModule,],
-  controllers: [],
+  controllers: [AppController], 
   providers: [PrismaService],
 })
 export class AppModule {}
